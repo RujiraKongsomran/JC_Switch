2c3cab435470ddac6862515e171e153a14bd457a
Rujira Kongsomran <rujirabiwberry@gmail.com>
2023-09-26 20:35:55 +0700

    Switch minimal

--- app/src/main/java/com/rujirakongsomran/jc_switch/MainActivity.kt
+++ app/src/main/java/com/rujirakongsomran/jc_switch/MainActivity.kt
@@ -6,7 +6,14 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
+import androidx.compose.material3.Switch
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
 import com.rujirakongsomran.jc_switch.ui.theme.JC_SwitchTheme
 
 class MainActivity : ComponentActivity() {
@@ -24,5 +31,18 @@ class MainActivity : ComponentActivity() {
             }
         }
     }
+}
+
+@Preview(showSystemUi = true)
+@Composable
+fun SwitchMinimalExample() {
+    var checked by remember { mutableStateOf(true) }
+
+    Switch(
+        checked = checked,
+        onCheckedChange = {
+            checked = it
+        }
+    )
 }
 
